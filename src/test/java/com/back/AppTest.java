d2aa079ed6f559d7ae96c9555e2ac8cac74a9ba9
user.email <younsoo6103@naver.com>
2025-05-29 15:22:34 +0900

    step 2, r

--- src/test/java/com/back/AppTest.java
+++ src/test/java/com/back/AppTest.java
@@ -6,11 +6,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
     @Test
-    @DisplayName("")
+    @DisplayName("1 + 2 == 3")
     void t1(){
         App app = new App();
         int rs = app.plus(1,2);
 
         assertEquals(3, rs);
+    }
+
+    @Test
+    @DisplayName("10 + 20 == 30")
+    void t2(){
+        App app = new App();
+        int rs = app.plus(10,20);
+
+        assertEquals(30, rs);
     }
 }
