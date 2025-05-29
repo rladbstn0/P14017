0826963787e2186ade1d8494794dce98d70eef2f
user.email <younsoo6103@naver.com>
2025-05-29 15:20:40 +0900

    step 1

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -4,4 +4,8 @@ public class App {
     public void run() {
         System.out.println("계산기");
     }
+
+    public int plus(int num1, int num2) {
+        return 3;
+    }
 }
--- src/test/java/com/back/AppTest.java
+++ src/test/java/com/back/AppTest.java
@@ -2,11 +2,15 @@ package com.back;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
     @Test
-    @DisplayName("테스트 1")
+    @DisplayName("")
     void t1(){
+        App app = new App();
+        int rs = app.plus(1,2);
 
+        assertEquals(3, rs);
     }
 }
